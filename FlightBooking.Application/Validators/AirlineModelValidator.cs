2921975a0e4ecdc5a00266231702d1ff3654db06
bArtOzzzz <breakyourbrain55@gmail.com>
2023-01-16 10:18:46 +0300

    Refactoring

---
+++ FlightBooking.Application/Validators/AirlineModelValidator.cs
@@ -0,0 +1,16 @@
+﻿using FlightBooking.Application.Dto;
+using FluentValidation;
+
+namespace FlightBooking.API.Validators
+{
+    public class AirlineModelValidator : AbstractValidator<AirlineDto>
+    {
+        public AirlineModelValidator()
+        {
+            RuleFor(a => a.AirlineName).NotNull().NotEmpty();           
+
+            RuleFor(a => a.Rating).InclusiveBetween(0.0, 5.0)
+                                  .WithMessage("Rating can be only between 0.0 and 5.0");
+        }
+    }
+}
--- FlightBooking.UI/Program.cs
+++ FlightBooking.UI/Program.cs
@@ -5,7 +5,11 @@ using FlightBooking.Infrastructure.Repository;
 using FlightBooking.Application.Services;
 using FlightBooking.Application.Mapper;
 using Microsoft.EntityFrameworkCore;
+using FlightBooking.Application.Dto;
 using FlightBooking.Infrastructure;
+using FlightBooking.API.Validators;
+using FluentValidation.AspNetCore;
+using FluentValidation;
 using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -22,14 +26,19 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddScoped<IAirlineRepository, AirlineRepository>();
 builder.Services.AddScoped<IAirlineService, AirlineService>();
 
+builder.Services.AddScoped<IValidator<AirlineDto>, AirlineModelValidator>();
+
 builder.Services.AddSwaggerGen();
+
+// Add Fluent Validation
+builder.Services.AddFluentValidationClientsideAdapters();
 
 builder.Services.AddMediatR(typeof(GetAllAirlinesQuery).Assembly);
  
 // Database connection
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("FlightBooking.Infrastructure"));
+    options.UseSqlServer(builder.Configuration["DefaultConnectionToLocalDatabase"], b => b.MigrationsAssembly("FlightBooking.Infrastructure"));
 });
 
 var app = builder.Build();
